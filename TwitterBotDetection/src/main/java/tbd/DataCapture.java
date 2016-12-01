65ac970faf76aed79c0caf3e7b4c64af64e8a199
Adam Fleming <2074813f@student.gla.ac.uk>
2016-12-01 14:35:32 +0000

    Changed package name.

--- TwitterBotDetection/src/main/java/tbd/DataCapture.java
+++ TwitterBotDetection/src/main/java/tbd/DataCapture.java
@@ -1,3 +1,4 @@
+package tbd;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
--- TwitterBotDetection/src/main/java/tbd/TwitterBotDetection.java
+++ TwitterBotDetection/src/main/java/tbd/TwitterBotDetection.java
@@ -1,3 +1,4 @@
+package tbd;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
